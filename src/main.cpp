02771d6552f6199f12f4490b091aa62e6bd44bfa
Joseph Shunk <joseph.shunk@gmail.com>
2023-08-13 05:45:08 -0700

    Added requirement of C++20

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 // System Libraries:
 #include <iostream>
+#include <concepts>
+
+template<class T>
+requires std::integral<T>
+bool IsIntegral()
+{
+	return true;
+}
+
+template<class T>
+bool IsIntegral()
+{
+	return false;
+}
 
 int main()
 {
-	std::cout << "Hello, World!\n";
+	if (IsIntegral<int>())
+	{
+		std::cout << "Hello, World!\n";
+	}
+	else
+	{
+		std::cout << "Not an integral type!\n";
+	}
 	return 0;
 }
